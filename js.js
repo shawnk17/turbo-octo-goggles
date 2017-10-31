c68f078abf2945912f3a96b4a245dc7c62840f56
shawn <axe2781@gmail.com>
2017-10-31 10:10:24 -0400

    l

--- js.js
+++ js.js
@@ -8,40 +8,40 @@ displayDiv = document.getElementById("customer-list"),
 nextId = 0,
 customerList = [];
 
-phrases1 = ["Once upon a time",
-"In a small village by the sea",
-"Long ago and far away",
-"In a world",
-"A long time ago, in a galaxy far, far away"],
-phrases2 = ["lived someone named",
-"there lived a person called",
-"a mad scientist created a hideous monster called",
-"lived a beautiful beast named",
-"existed an ancient alien who went by the odd name of"],
+phrases1 = [" ",
+" ",
+" ",
+" ",
+" "],
+phrases2 = [" ",
+" ",
+" ",
+" ",
+" "],
 phrases3 = [
-"who ate",
-"who was eaten by",
-"who fell in love with",
-"who was betrayed by",
-"who loved to play quietly with",
-"who killed"],
+" ",
+" ",
+" ",
+" ",
+" ",
+" "],
 
 phrases4 = [
-"an apple",
-"a  dragon",
-"a scary ghost",
-"a beautiful young woman",
-"a handome young man",
-"Superman",
-"a puzzle with a picture of a lovely river",
-"a horrifying gorgon"];
+" ",
+" ",
+" ",
+" ",
+" ",
+" ",
+" ",
+" "];
 
 var points = [id, firstName, lastName, email];
-points.sort(function(a, b){return a - b});
+points.sort(function(a, b, c, d){return a - b, c-d});
 
 var getPhrase = function getPhrase(wordList){
-var randy = getRandomNumber(0, wordList.length - 1);
-return wordList[randy];
+var shawn = getRandomNumber(0, wordList.length - 1);
+return wordList[shawn];
 }
 
 makeButton.addEventListener("click", function(){
@@ -60,32 +60,25 @@ resultsArea.innerHTML = "";
 userName.value = "";
 });
 
-var getRandomNumber = function getRandomNumber(min, max){
-min = Math.ceil(min);
-max = Math.floor(max);
-var randy = Math.random();
-
-return Math.floor(randy * (max - min + 1) + min);
-}
-
-var Customer = function Customer(firstName, lastName, salutation){
+var Customer = function Customer(firstName, lastName, id, semial){
 this.firstName = firstName;
 this.lastName = lastName;
-this.salutation = salutation;
+this.id = id;
+this.email = email;
 this.id = nextId++;
 
 this.greetFormally = function greetFormally(){
     var greetString = "Greetings ";
 
-    if(this.salutation == ""){
+    if(this.id == ""){
         greetString += this.firstName + " " + this.lastName;
     } else {
-        greetString += this.salutation + " " + this.lastName;
+        greetString += this.id + " " + this.lastName;
+        greetString += this.email + " " + this.lastName;
     }
 
     return greetString;
-    } 
-}
+     
 
 var renderCustomers = function renderCustomers(){
 displayDiv.innerHTML = " ";
@@ -99,23 +92,14 @@ for(var i = 0; i < customerList.length; i++){
 var clearInputs = function clearInputs(){
 firstName.value = " ";
 lastName.value = " ";
-salutation.value = " ";
+id.value = " ";
+email.value = " ";
 };
 
-addButton.addEventListener("click", function(){
-if(firstName.value == " " || lastName.value == " ")
-{
-    alert("Hey! You didn't enter a first name or last name!");
-    return;
-}
 var newCustomer = new Customer(firstName.value,
-    lastName.value, salutation.value);
+    lastName.value, id.value, email.value);
 
 customerList.push(newCustomer);
-
-clearInputs();
-renderCustomers();
-});
 
 clearButton.addEventListener("click", function(){
 customerList = [];
